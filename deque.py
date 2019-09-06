2159d23fa50593d096e9730c43af9f34f12eed1e
twheelertech <tony@wheelertechconsulting.com>
2019-09-06 01:26:37 -0500

    Added deque  Class

--- deque.py
+++ deque.py
@@ -5,3 +5,39 @@ Created on Thu Sep  5 22:26:21 2019
 @author: tony
 """
 
+class Deque(object):
+    def __init__(self):
+        self.items = []
+        
+    def isEmpty(self):
+        return self.items == []
+    
+    def addFront(self,item):
+        self.items.append(item)
+        
+    def addRear(self,item):
+        self.items.insert(0,item)
+        
+    def removeFront(self):
+        return self.items.pop()
+    
+    def removeRear(self):
+        return self.items.pop(0)
+    
+    def size(self):
+        len(self.items)
+        
+
+d = Deque()
+
+d.addFront('ninja')
+d.addFront('night')
+d.addRear('moonlight')
+
+d.size()
+
+print(d.removeFront() + ' ' + d.removeRear())
+
+d.isEmpty()
+
+d.removeFront()
