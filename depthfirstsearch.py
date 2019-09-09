c1ef217d7e45ad8005c15d74660dc9f19d9f7a4f
twheelertech <tony@wheelertechconsulting.com>
2019-09-09 01:58:11 -0500

    Added graphimplementation

--- depthfirstsearch.py
+++ depthfirstsearch.py
@@ -1,47 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep  9 01:03:22 2019
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
 
-@author: tony
-"""
-
-from enum import Enum
-from collections import OrderedDict 
-class State(Enum):
-    unvisited = 1
-    visited = 2
-    visiting = 3
+def dfs(graph,start):
+    visited = set()
+    stack = [start]
     
-class Node:
-    def __init__(self,num):
-        self.num = num 
-        self.visit_state = State.unvisited
-        self.adjacent = OrderedDict()
+    while stack:
+        vertex = stack.pop()
         
-    def __str__(self):
-        return str(self.num)
-
-class Graph:
-    def __init__(self):
-        self.nodes = OrderedDict()
-        
-        
-    def add_node(self,num):
-        node = Node(num)
-        self.nodes[num] = node
-        return node
-    
-    def add_edge(self,source,dest,weight=0):
-        if source not in self.nodes:
-            self.add_node(source)
+        if vertex not in visited:
+            visited.add(vertex)
             
-        if dest not in self.nodes:
-            self.add_node(dest)
-        self.nodes[source].adjacent[self.nodes[dest]] = weight
+            stack.extend(graph[vertex] - visited)
+            
+    return visited
 
-g = Graph()
-
-g.add_edge(0,1,5)
-g.add_edge(1,2,4)
-
-g.nodes        
+dfs(graph,'A')
